8506bb3884fec9eb533ad3a03df0e470f95b9773
TomStanko <ef9934@wayne.edu>
2014-06-02 15:26:31 -0400

    Made some changes to the layout

--- GarageSale/startpage/src/main/java/com/adaptivelite/garagesale/startpage/MainActvity.java
+++ GarageSale/startpage/src/main/java/com/adaptivelite/garagesale/startpage/MainActvity.java
@@ -17,7 +17,7 @@ public class MainActvity extends Activity{
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main_actvity, menu);
+        getMenuInflater().inflate(R.menu.main_actvity, menu);
         return true;
     }
 
